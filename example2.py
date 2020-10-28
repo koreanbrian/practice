612aa9a48a92fcfdbce505d1361638e0bbe7a963
koreanbrian <jhy2139@gmail.com>
2020-10-28 19:07:09 +0900

    commit

--- example2.py
+++ example2.py
@@ -1,6 +1,26 @@
-def dfs(graph,v,visited):
-    visited[v]=True
-    print(v, end=' ')
+from collections import deque
 
-    for i in graph[v]:
-        
+def bfs(graph, start, visited):
+    queue = deque([start])
+
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]s:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],
+    [[2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
